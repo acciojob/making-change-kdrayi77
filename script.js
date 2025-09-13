9f876e8d32b962ccac324f53978e167b565311f7
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-09-13 10:30:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const makeChange = (c) => {
-  // your name here
+	const A={};
+ A.q=Math.floor(c/25);
+	c=c%25;
+	A.d=Math.floor(c/10);
+	c=c%10;
+  A.n=Math.floor(c/5);
+	c=c%5;
+	A.p=Math.floor(c/1);
+	return A;
 };
 
 // Do not the change the code below
